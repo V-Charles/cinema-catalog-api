7835183d66042591fe0683e56e69f204065a9850
Vinicius Charles <viniciuscharles3101@gmail.com>
2026-06-18 18:39:20 -0300

    fix(frontend): corrige persistencia do tema escuro usando localStorage

--- src/main/resources/static/theme.js
+++ src/main/resources/static/theme.js
@@ -1,32 +1,10 @@
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = cname = "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
 function carregarTema() {
-    let tema = getCookie("tema");
+    let tema = localStorage.getItem("tema");
     if (tema === "escuro") {
         document.body.classList.add("dark-mode");
         document.getElementById("btn-tema").innerText = "Tema Claro";
     } else {
+        document.body.classList.remove("dark-mode");
         document.getElementById("btn-tema").innerText = "Tema Escuro";
     }
 }
@@ -36,10 +14,10 @@ function alternarTema() {
     element.classList.toggle("dark-mode");
 
     if (element.classList.contains("dark-mode")) {
-        setCookie("tema", "escuro", 30);
+        localStorage.setItem("tema", "escuro");
         document.getElementById("btn-tema").innerText = "Tema Claro";
     } else {
-        setCookie("tema", "claro", 30);
+        localStorage.setItem("tema", "claro");
         document.getElementById("btn-tema").innerText = "Tema Escuro";
     }
 }
