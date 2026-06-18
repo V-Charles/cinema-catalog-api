d58d0166155225f7b34efb1e7d8749669c7cec89
Vinicius Charles <viniciuscharles3101@gmail.com>
2026-06-18 16:35:52 -0300

    chore(config): configura banco de dados h2 para testes locais

--- src/main/java/br/com/casacultural/cinema/CinemaApplication.java
+++ src/main/java/br/com/casacultural/cinema/CinemaApplication.java
@@ -2,8 +2,8 @@ package br.com.casacultural.cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class CinemaApplication extends SpringBootServletInitializer {
@@ -16,5 +16,4 @@ public class CinemaApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaApplication.class, args);
 	}
-
 }
