13ee6adb2cfa99507b8a0144eb49a52429c3401a
Vinicius Charles <viniciuscharles3101@gmail.com>
2026-06-21 13:22:28 -0300

    chore(infra): adiciona endpoint health-check e variáveis de ambiente para o mysql

---
+++ src/main/java/br/com/casacultural/cinema/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package br.com.casacultural.cinema.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> insone() {
+        return ResponseEntity.ok("OK");
+    }
+}
